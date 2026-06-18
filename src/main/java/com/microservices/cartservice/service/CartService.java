ae6b436c3859b3a279a23af3b5768530c2bcde3c
Akhila Reddy Baddam <akhilareddybaddam@Akhila-Reddys-MacBook-Pro.local>
2026-06-18 19:34:11 -0400

    1D-service-layer

--- src/main/java/com/microservices/cartservice/service/CartService.java
+++ src/main/java/com/microservices/cartservice/service/CartService.java
@@ -1,6 +1,8 @@
 package com.microservices.cartservice.service;
 
 import com.microservices.cartservice.entity.Cart;
+import com.microservices.cartservice.entity.CartItem;
+import com.microservices.cartservice.repository.CartItemRepository;
 import com.microservices.cartservice.repository.CartRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,11 @@ import java.util.List;
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
-    public CartService(CartRepository cartRepository) {
+    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository) {
         this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
     }
 
     public Cart addCart(Cart cart) {
@@ -21,5 +25,13 @@ public class CartService {
 
     public List<Cart> getAllCarts() {
         return cartRepository.findAll();
+    }
+
+    public CartItem addCartItem(CartItem cartItem) {
+        return cartItemRepository.save(cartItem);
+    }
+
+    public List<CartItem> getAllCartItems() {
+        return cartItemRepository.findAll();
     }
 }
